5bd38c1169477638316ffffd46028de7c436c07a
Christoph <christoph.geitner@gmail.com>
2017-05-31 13:14:45 +0200

    some changes in person.h and initial upload of person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,27 @@
+#include <string>
+#include "Person.h"
 
+using namespace std;
+
+Person::Person(string vorname, string nachname, Datum geburtsdatum, Geschlecht)
+{
+    d_vorname = vorname;
+    d_nachname = nachname;
+    d_geburtsdatum = geburtsdatum;
+    d_geschlecht = geschlecht;
+}
+
+void Person::printName()
+{
+    cout << d_vorname << " " << d_nachname;
+}
+
+Datum Person::getGeburtsdatum()
+{
+    return d_geburtsdatum;
+}
+
+Geschlecht Person::getGeschlecht()
+{
+    return d_geschlecht;
+}
--- Person.h
+++ Person.h
@@ -1,28 +1,28 @@
-#ifndef _PERSON_H_
-#define _PERSON_H_
+#ifndef PERSON_H
+#define PERSON_H
 
 #include <string>
+#include "Datum.h"
 
 enum Geschlecht
 {
     male,
     female,
-    undefined
+    unknown
 };
 
 class Person
 {
     std::string d_vorname, d_nachname;
     Datum d_geburtsdatum;
-    enum Geschlecht d_geschlecht;
+    Geschlecht d_geschlecht;
 
 public:
-    Person();
     Person(std::string, std::string, Datum, enum Geschlecht);
     void printName();
     Datum getGeburtsdatum();
-    enum Geschlecht getGeschlecht();
+    Geschlecht getGeschlecht();
 };
 
-#endif //_PERSON_H_
+#endif //PERSON_H
 
