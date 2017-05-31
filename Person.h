f5a44fd81f414982cbe112deb943a7d6b87d3226
Christoph <christoph.geitner@gmail.com>
2017-05-31 13:00:43 +0200

    Erstellen der Klasse Person und der enum Geschlecht

--- Person.h
+++ Person.h
@@ -1,1 +1,28 @@
+#ifndef _PERSON_H_
+#define _PERSON_H_
+
+#include <string>
+
+enum Geschlecht
+{
+    male,
+    female,
+    undefined
+};
+
+class Person
+{
+    std::string d_vorname, d_nachname;
+    Datum d_geburtsdatum;
+    enum Geschlecht d_geschlecht;
+
+public:
+    Person();
+    Person(std::string, std::string, Datum, enum Geschlecht);
+    void printName();
+    Datum getGeburtsdatum();
+    enum Geschlecht getGeschlecht();
+};
+
+#endif //_PERSON_H_
 
