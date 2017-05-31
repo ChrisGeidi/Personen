1a3c1222fd626cce64e0ba72cb32a92a074ec192
jereimer <jens@reimer-warendorf.de>
2017-05-31 14:27:24 +0200

    Anfänge Datum

--- Datum.cpp
+++ Datum.cpp
@@ -1,1 +1,16 @@
 
+#include "Datum.h"
+#include <string>
+
+using namespace std;
+
+Datum::Datum(int Tag, int Monat, int Jahr)
+{
+    d_Tag=Tag;
+    d_Monat=Monat;
+    d_Jahr=Jahr;
+}
+string Datum::getDatum()
+{
+    return Datum
+}
--- Datum.h
+++ Datum.h
@@ -1,1 +1,21 @@
+#ifndef DATUM_H
+#define DATUM_H
 
+#include <string>
+using namespace std;
+
+class Datum : Datum
+{
+    Datum(int,int,int);
+    int d_Tag;
+    int d_Monat;
+    int d_Jahr;
+
+public:
+    int getTag();
+    int getMonat();
+    int getJahr();
+};
+
+
+#endif // DATUM
