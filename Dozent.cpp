1722769344f40520693ec80f86f9d6bd8ae2b706
cbevermann <carsten.bevermann@hs-osnabrueck.de>
2017-05-31 13:26:20 +0200

    class Dozent erstellt

--- Dozent.cpp
+++ Dozent.cpp
@@ -1,1 +1,17 @@
+#include "Dozent.h"
 
+Dozent::Dozent(std::string Hs, std::string Fr)
+{
+    d_Hochschule=Hs;
+    d_Fachrichtung=Fr;
+}
+
+std::string Dozent::getHochschule()
+{
+    return d_Hochschule;
+}
+
+std::string Dozent::getFachrichtung()
+{
+    return d_Fachrichtung;
+}
--- Dozent.h
+++ Dozent.h
@@ -1,1 +1,20 @@
+#ifndef _DOZENT_H_
+#define _DOZENT_H_
 
+#include <iostream>
+
+class Dozent
+{
+    std::string d_Hochschule;
+    std::string d_Fachrichtung;
+
+    public:
+    Dozent(std::string,std::string);
+    std::string getHochschule();
+    std::string getFachrichtung();
+};
+
+
+
+
+#endif // _DOZENT_H_
