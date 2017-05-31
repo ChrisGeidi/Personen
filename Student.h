e48757e1b615775250830a74d3855871f99a231b
MarcToepker <marc.toepker@hs-osnabrueck.de>
2017-05-31 13:05:35 +0200

    Bearbeiten der Student.h Datei

--- Student.h
+++ Student.h
@@ -1,1 +1,17 @@
 
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+class Student
+{
+    int d_Matrikelnummer;
+    string d_Hochschule;
+    string d_Studiengang;
+public:
+    int getMatrikelnummer();
+    string getHochschule();
+    string getStudiengang();
+};
+
+#endif // STUDENT_H
